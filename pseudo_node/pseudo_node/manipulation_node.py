bebc018d4bd285419bb044b7d347e6899a931a77
ItoMasaki <40968251+ItoMasaki@users.noreply.github.com>
2022-04-13 11:54:08 +0900

    Update manipulation_node.py

--- pseudo_node/pseudo_node/manipulation_node.py
+++ pseudo_node/pseudo_node/manipulation_node.py
@@ -14,18 +14,16 @@ class ManipulationServer(Node):
             StringCommand, 'manipulation/command', self.command_callback)
 
     def command_callback(self, request, response):
-        self.get_logger().info(f"{request.command}")
-
         sleep(1)
         
         prob = random.random()
-        self.get_logger().info(f"Try to manipulate the target object")
+        self.get_logger().info(f"目標の物体を把持します")
 
         if 0.7 > prob:
-            self.get_logger().info("Succeeded to manipulate the target object")
-            response.answer = "succeeded"
+            self.get_logger().info("目標物体の把持が成功しました")
+            response.answer = "reached"
         else:
-            self.get_logger().info("Failed to manipulate the target object")
+            self.get_logger().info("目標物体の把持が失敗しました")
             response.answer = "failed"
 
         return response
