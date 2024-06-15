9a11416af2a82875eafd411a1d9e9fe7fc0776ea
MrKeith99 <kvalentincardenas@hotmail.com>
2024-06-15 22:31:10 +0900

    [UPD] max eat is required

--- sample_sm_flexbe/sample_sm_flexbe_states/sample_sm_flexbe_states/search_state.py
+++ sample_sm_flexbe/sample_sm_flexbe_states/sample_sm_flexbe_states/search_state.py
@@ -24,29 +24,30 @@ from time import sleep
 
 class SearchState(EventState):
     """
-    SearchStateという状態はスナックを探すことを目標としています．
+    SearchStateという状態はスイーツを探すことを目標としています．
     ユーザーがこれまでどれぐらい食べたかによって，食べるか食べないかを判定します．
 
     出力
-    <= succeeded       スナックが見つけたら，検出成功するという結果を出力します
-    <= finished        スナックでお腹いっぱいになったら，検索終了の結果を出力します
+    <= succeeded       スイーツが見つけたら，検出成功するという結果を出力します
+    <= finished        スイーツでお腹いっぱいになったら，検索終了の結果を出力します
     <= failed          何らかの問題で，検索を失敗した場合，失敗したという結果を出力します
 
     Userdata
-    ># eat_counter  int ユーザーがこれまでまで食べたスナックの数(int型) (Input)
+    ># eat_counter  int ユーザーがこれまでまで食べたスイーツの数       (int型) (Input)
+    ># max_eat      int ユーザーがお腹いっぱいになるまでのスイーツの数 (int型) (Input)
     """
 
     def __init__(self):
         """状態の結果，入力キーを定義します．"""
         super().__init__(outcomes=['succeeded', 'finished'],
-                         input_keys=['eat_counter'])
+                         input_keys=['eat_counter', 'max_eat'])
 
     def execute(self, userdata):
         # search処理を開始します
         sleep(1)
         Logger.loginfo('スイーツを探索しています') # 探索の状態にいることをログに残します
 
-        if userdata.eat_counter < 3:
+        if userdata.eat_counter < userdata.max_eat:
             Logger.loginfo('スイーツを見つけました！') # 探索の状態へ訪れた回数が3回未満の場合
 
             return 'succeeded' # 'succeeded'という結果を返します
